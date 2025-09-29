90c94d64d3984db1bf01efb2727da7c07f5dee21
Karan <karankgudha@gmail.com>
2025-09-29 20:52:51 +0530

    add inventory route

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 import productRoute from "./routes/product.Routes.js"
+import inventoryRoute from "./routes/inventory.Routes.js"
 app.use("/api/v1/product", productRoute);
+app.use("/api/v1/inventory", inventoryRoute);
 
 export { app };
